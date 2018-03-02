58b811989c9cb5a022bb0829730ed1bacec99d0d
Puspa Khanal <31549620+puspakhanal70726@users.noreply.github.com>
2018-03-02 14:35:43 -0500

    make it better

--- db/migrate/20180301013931_create_movies.rb
+++ db/migrate/20180301013931_create_movies.rb
@@ -1,7 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.1]
   def change
     create_table :movies do |t|
-      t:string :image
+      t.string :image
       t.string :title
       t.date :release_date
       t.string :director
